0a90d7b329737e274b1f88cfd997644176af2612
Noraphat Chatthin <noraphat@gmail.com>
2025-03-01 08:22:23 +0700

    feat(backend): Add CORESMiddleware for send data to api

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,8 @@
 # main.py
 from fastapi import FastAPI
 from routers import user as user_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # 🚀 สร้าง FastAPI App
 app = FastAPI(
@@ -11,3 +13,10 @@ app = FastAPI(
 
 # 🔗 รวม Router เข้ากับ FastAPI App
 app.include_router(user_router.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # สามารถเปลี่ยนเป็น ["http://localhost:5500"] ตาม Origin ของ Frontend ได้
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
